50e9196061cd7314a72a0620970142cd41b95e5a
Angie Lozano de arevalo <anyilozano2109@gmail.com>
2020-08-26 23:49:06 -0500

    finalizados

--- src/views/Datos/components/formulario.vue
+++ src/views/Datos/components/formulario.vue
@@ -5,43 +5,51 @@
                 <v-col md="6">
                     <v-col md="12">
                         <label>Nombre</label><br>
-                        <input type="text" class="nameForm" placeholder="Tu nombre"/>
+                        <input type="text" v-model.trim="user.name" class="nameForm" placeholder="Tu nombre"/>
+                        <small v-if="errorName">Tu nombre no puede llevar números o caracteres especiales</small>
                     </v-col>
                     <v-col md="12" style="margin-bottom: 38px">
                         <label>Apellido</label><br>
-                        <input type="text" class="nameForm" placeholder="Tu apellido"/>
+                        <input type="text" v-model.trim="user.lastName" class="nameForm" placeholder="Tu apellido"/>
+                        <small v-if="errorLastName">Tu apellido no puede llevar números o caracteres especiales</small>
                     </v-col>
                     <v-divider style="margin-bottom: 25px"></v-divider>
                     <v-col md="12">
                         <label>Email</label><br>
-                        <input type="text" class="nameForm" placeholder="Tu email"/>
+                        <input type="email" v-model.trim="user.email" class="nameForm" placeholder="Tu email"/>
+                        <small v-if="errorEmail">Correo electronico invalido</small>
                     </v-col>
                     <v-col md="12" style="margin-bottom: 38px">
                         <label>Telefono</label><br>
-                        <input type="text" class="nameForm" placeholder="Tu telefono"/>
+                        <input type="number" v-model.trim="user.phone" class="nameForm" placeholder="Tu telefono"/>
+                        <small v-if="errorPhone">Número de telefono incorrecto, solo puede llevar números</small>
                     </v-col>
                     <v-divider style="margin-bottom: 25px"></v-divider>
                     <v-col md="12">
                         <label>Fecha de nacimiento</label><br>
                         <v-row>
                             <v-col md="4">
-                                <input type="text" class="dayForm" placeholder="dd"/>
+                                <input type="number" v-model.trim="user.day"  class="dayForm" placeholder="dd"/>
                             </v-col>
                             <v-col md="4">
-                                <input type="text" class="dayForm" placeholder="mm"/>
+                                <input type="number" v-model.trim="user.month" class="dayForm" placeholder="mm"/>
                             </v-col>
                             <v-col md="4">
-                                <input type="text" class="dayForm" placeholder="aaaa"/>
+                                <input type="number" v-model.trim="user.year" class="dayForm" placeholder="aaaa"/>
                             </v-col>
                         </v-row>
+                        <small v-if="errorDay">El día solo puede llevar números</small>
+                        <small v-if="erroMonth">Solo ingresa el número del mes</small>
+                        <small v-if="errorYear">Ingresa los 4 digitos de tu año de nacimiento</small>
                     </v-col>
                     <v-col md="12">
                         <label>Ciudad</label><br>
-                        <input type="text" class="nameForm" placeholder="Tu ciudad"/>
+                        <input type="text" v-model.trim="user.city" class="nameForm" placeholder="Tu ciudad"/>
+                        <small v-if="errorCity">La ciudad donde te encuentras no puede contener números.</small>
                     </v-col>
                     <v-col md="12">
                         <label>Dirección</label><br>
-                        <input type="text" class="nameForm" placeholder="Tu dirección"/>
+                        <input type="text" v-model.trim="user.address" class="nameForm" placeholder="Tu dirección"/>
                     </v-col>
                 </v-col>
                 <v-col md="6">
@@ -52,9 +60,9 @@
                                 <v-checkbox v-model="poli" class="mx-2 checkboxDatos" label="Acepta nuestra Política de privacidad"></v-checkbox>
                             </v-col>
                             <v-col md="12" align="center" justify="center">
-                                <router-link to="/suscribete" style="text-decoration: none;">                            
-                                    <v-btn class="trarme"><span class="lep">Registrarme</span></v-btn>
-                                </router-link>
+                                <!-- <router-link to="/suscribete" style="text-decoration: none;">                             -->
+                                    <v-btn class="trarme" @click="validateInputs"><span class="lep" >Registrarme</span></v-btn>
+                                <!-- </router-link> -->
                             </v-col>
                         </v-card>
                     </v-col>
@@ -62,45 +70,53 @@
             </v-row>
         </v-container>
         <v-container class="formResponsive">
-            <v-col md="12">
+             <v-col md="12">
                 <label>Nombre</label><br>
-                <input type="text" class="nameForm" placeholder="Tu nombre"/>
+                <input type="text" v-model.trim="user.name" class="nameForm" placeholder="Tu nombre"/>
+                <small v-if="errorName">Tu nombre no puede llevar números o caracteres especiales</small>
             </v-col>
             <v-col md="12" style="margin-bottom: 38px">
                 <label>Apellido</label><br>
-                <input type="text" class="nameForm" placeholder="Tu apellido"/>
+                <input type="text" v-model.trim="user.lastName" class="nameForm" placeholder="Tu apellido"/>
+                <small v-if="errorLastName">Tu apellido no puede llevar números o caracteres especiales</small>
             </v-col>
             <v-divider style="margin-bottom: 25px"></v-divider>
             <v-col md="12">
                 <label>Email</label><br>
-                <input type="text" class="nameForm" placeholder="Tu email"/>
+                <input type="email" v-model.trim="user.email" class="nameForm" placeholder="Tu email"/>
+                <small v-if="errorEmail">Correo electronico invalido</small>
             </v-col>
             <v-col md="12" style="margin-bottom: 38px">
                 <label>Telefono</label><br>
-                <input type="text" class="nameForm" placeholder="Tu telefono"/>
+                <input type="number" v-model.trim="user.phone" class="nameForm" placeholder="Tu telefono"/>
+                <small v-if="errorPhone">Número de telefono incorrecto, solo puede llevar números</small>
             </v-col>
             <v-divider style="margin-bottom: 25px"></v-divider>
             <v-col md="12">
                 <label>Fecha de nacimiento</label><br>
                 <v-row>
                     <v-col md="4">
-                        <input type="text" class="nameForm" placeholder="dd"/>
+                        <input type="number" v-model.trim="user.day"  class="dayForm" placeholder="dd"/>
                     </v-col>
                     <v-col md="4">
-                        <input type="text" class="nameForm" placeholder="mm"/>
+                        <input type="number" v-model.trim="user.month" class="dayForm" placeholder="mm"/>
                     </v-col>
                     <v-col md="4">
-                        <input type="text" class="nameForm" placeholder="aaaa"/>
+                        <input type="number" v-model.trim="user.year" class="dayForm" placeholder="aaaa"/>
                     </v-col>
                 </v-row>
+                <small v-if="errorDay">El día solo puede llevar números</small>
+                <small v-if="erroMonth">Solo ingresa el número del mes</small>
+                <small v-if="errorYear">Ingresa los 4 digitos de tu año de nacimiento</small>
             </v-col>
             <v-col md="12">
                 <label>Ciudad</label><br>
-                <input type="text" class="nameForm" placeholder="Tu ciudad"/>
+                <input type="text" v-model.trim="user.city" class="nameForm" placeholder="Tu ciudad"/>
+                <small v-if="errorCity">La ciudad donde te encuentras no puede contener números.</small>
             </v-col>
             <v-col md="12">
                 <label>Dirección</label><br>
-                <input type="text" class="nameForm" placeholder="Tu dirección"/>
+                <input type="text" v-model.trim="user.address" class="nameForm" placeholder="Tu dirección"/>
             </v-col>
             <v-col md="12" style="margin-bottom: 38px">
                 <v-card class="cardDatos">
@@ -109,10 +125,7 @@
                         <v-checkbox v-model="poli" class="mx-2 checkboxDatos" label="Acepta nuestra Política de privacidad"></v-checkbox>
                     </v-col>
                     <v-col md="12">
-                        <router-link to="/suscribete" class="tion" style="text-decoration: none;">
-            
-                            <v-btn class="trarme"><span class="lep">Registrarme</span></v-btn>
-                        </router-link>
+                        <v-btn class="trarme"><span class="lep" @click="validateInputs">Registrarme</span></v-btn>
                     </v-col>
                 </v-card>
             </v-col>
@@ -123,7 +136,51 @@
 export default {
     data: () =>({
         data: false,
-        poli: false
-    })
+        poli: false,
+        ruleString: /[A-Za-z ñ]+/,
+        ruleEmail: /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i,
+        ruleNumber: /^([0-9])*$/,
+        user: {
+            name: '',
+            lastName: '',
+            email: '',
+            phone: null,
+            day: null,
+            month: null,
+            year: null,
+            city: '',
+            address: ''
+        },
+        errorName: false,
+        errorLastName: false,
+        errorEmail: false,
+        errorPhone: false,
+        erroMonth: false,
+        errorYear: false,
+        errorDay: false,
+        errorCity: false,
+        errorAddress: false,
+    }),
+    methods: {
+        validateInputs(){
+
+            console.log(!this.ruleNumber.test(this.user.phone))
+
+
+            if(!this.ruleString.test(this.user.name)){
+                this.errorName = true
+            }else if(!this.ruleString.test(this.user.lastName)){
+                this.errorName = false
+                this.errorLastName = true
+            }else if(!this.ruleEmail.test(this.user.email)){
+                this.errorLastName = false
+                this.errorEmail = true
+            }else if(!this.ruleString.test(this.user.city)){
+                this.errorCity = true
+            }else{
+                this.$router.push('/suscribete')
+            }
+        }
+    }
 }
 </script>
